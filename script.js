7b623cdba7737dca179b4ffc29fe202fc6c52470
Siddhartha Sarkar <siddsarkar7@gmail.com>
2022-09-16 15:27:23 +0530

    update

--- script.js
+++ script.js
@@ -3,9 +3,4 @@ window.addEventListener("load", onVrViewLoad);
 function onVrViewLoad() {
   var element = document.querySelector("dl8-video");
   element.start();
-
-  var vrView1 = new VRView.Player("#vr-view-1", {
-    video: "./file.mp4",
-    is_stereo: true,
-  });
 }
